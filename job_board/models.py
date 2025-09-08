481fefcefb72dee2a75e8fad83a5e108baf31729
oussama-dot <eloussamafouah@gmail.com>
2025-09-08 13:50:49 +0200

    last changes of job board app

--- job_board/models.py
+++ job_board/models.py
@@ -7,3 +7,6 @@ class JobPosting(models.Model):
     company = models.CharField(max_length=100)
     salary = models.IntegerField()
     is_active = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.title} {self.company} {self.salary} "
